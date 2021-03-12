0f7bc005ae3b8c5ec4340c128c6e7893aa6a3969
Lazar Bogdan <bogdilazar5@gmail.com>
2021-03-12 22:39:21 +0200

    Third stage

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Window;
+
 public class Controller {
+
+    @FXML
+    private Button Login;
+
+    @FXML
+    public void Login(ActionEvent event){
+        Window owner = Login.getScene().getWindow();
+
+        showAlert(owner);
+    }
+
+    private static void showAlert(Window owner) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.initOwner(owner);
+        alert.show();
+    }
+
 }
